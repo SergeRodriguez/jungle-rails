36b2b87cc7637aaa2b56eedda9476a2158ff0cd2
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-12-11 22:01:00 +0000

    create about page, add route for about page, add link to about page in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  get "/about" => "about#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
